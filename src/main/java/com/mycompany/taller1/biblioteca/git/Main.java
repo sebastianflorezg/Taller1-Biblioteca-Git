d1bce1427178db76a60aff9c8b0d91867a923523
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-08-14 12:20:09 -0500

    Agregar proyecto Biblioteca

---
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller1.biblioteca.git;
+
+/**
+ *
+ * @author sebas
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
