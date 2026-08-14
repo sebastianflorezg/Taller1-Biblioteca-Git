83ad38e99f42cfff739fdb7933d3c2e2621ba195
sebasflorezgomez <sflorezg@unicesar.edu.co>
2026-08-14 14:22:15 -0500

    feat: implementar CREATE de Customer

--- src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
+++ src/main/java/com/mycompany/taller1/biblioteca/git/Main.java
@@ -1,14 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.taller1.biblioteca.git;
+import java.util.ArrayList; 
+import java.util.Scanner;
 
-/**
- *
- * @author sebas
- */
 public class Main {
+    static ArrayList<Customer> customers = new ArrayList<>(); 
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void Create(Customer customer) {
+        customers.add(customer);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
